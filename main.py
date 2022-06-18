6a7a6ee59805e5d52f8c8914bf965ccea5251d31
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-06-18 13:09:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import requests
 import threading
 import random
+import json
+
 
 from colorama import Fore, init
 
 init(convert=True)
 
 
+with open('config.json') as f:
+    cfg = json.load(f)
+
+
+useproxy = cfg['use_proxy']
+
+
+readproxy = open('proxies.txt','r')
+readproxy2 = readproxy.readlines()
+workproxy = []
+for proxy3 in readproxy2:
+    proxystrip = proxy3.strip('\n')
+    workproxy.append(proxystrip)
+
+
+
 def check():
+
+    proxy1 = random.choice(workproxy)
+    proxies = {'http': f'http://{proxy1}','https':f'http://{proxy1}'}
 
     usersf = open("users.txt")
     user = random.choice(usersf.read().splitlines())
@@ -16,7 +37,10 @@ def check():
     header = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
     }
-    r = requests.get(f'https://www.tiktok.com/@{user}', headers=header)
+    if useproxy == True:
+        r = requests.get(f'https://www.tiktok.com/@{user}', headers=header, proxies=proxies)
+    else:
+        r = requests.get(f'https://www.tiktok.com/@{user}', headers=header)
     if r.status_code == 200:
         print(Fore.RED + f"Taken | {Fore.RESET}{user}\n")
     else:
